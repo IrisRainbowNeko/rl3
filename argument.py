8f7dff2a13d270242280a97cf1008f36f834b44f
dzy7e <228218809@qq.com>
2022-05-14 14:53:09 +0800

    update

--- argument.py
+++ argument.py
@@ -14,12 +14,12 @@ def dqn_arguments(parser):
     #parser.add_argument('--env_name', default="CartPole-v0", help='environment name')
 
     parser.add_argument("--seed", default=11037, type=int)
-    parser.add_argument("--buffer_size", default=int(3e4), type=int)
+    parser.add_argument("--buffer_size", default=int(5e4), type=int)
     parser.add_argument("--mem_step", default=int(5e3), type=int)
-    parser.add_argument("--lr", default=0.0002, type=float)
+    parser.add_argument("--lr", default=1e-4, type=float)
     parser.add_argument("--batch_size", default=1024, type=int)
-    parser.add_argument("--gamma", default=0.99, type=float)
-    parser.add_argument("--eps", default=0.9, type=float)
+    parser.add_argument("--gamma", default=0.95, type=float)
+    parser.add_argument("--eps", default=0.5, type=float)
     parser.add_argument("--eps_decay", default=[87000, 93000, 96000, 98000, 99000, 100000, 110000], type=str2list)
     parser.add_argument("--eps_decay_rate", default=0.8, type=str2list)
     parser.add_argument("--grad_norm_clip", default=10, type=float)
@@ -28,7 +28,7 @@ def dqn_arguments(parser):
     parser.add_argument("--use_cuda", default=True, type=bool)
     parser.add_argument("--n_frames", default=int(400000), type=int)
     parser.add_argument("--learning_freq", default=1, type=int)
-    parser.add_argument("--target_update_freq", default=5000, type=int)
+    parser.add_argument("--target_update_freq", default=10000, type=int)
 
     return parser
 
