39437deb03244d28ee6e9c6737656b659f01af05
dzy7e <228218809@qq.com>
2022-05-23 10:38:34 +0800

    update

--- agent_dir/agent_pg.py
+++ agent_dir/agent_pg.py
@@ -155,7 +155,7 @@ class AgentPG(Agent):
         Input:observation
         Return: action
         """
-        act = torch.softmax(self.Qnet(observation), dim=0).cpu().numpy()
+        act = torch.softmax(self.Qnet(observation), dim=0).cpu().view(-1).numpy()
         return np.random.choice(range(act.shape[0]), p=act)
 
     def run(self):
