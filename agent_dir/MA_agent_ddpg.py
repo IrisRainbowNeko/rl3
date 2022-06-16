fe5dc2cfd8a40184287a1f49ef246e864cf0a734
dzy7e <228218809@qq.com>
2022-06-16 19:43:37 +0800

    update

--- agent_dir/MA_agent_ddpg.py
+++ agent_dir/MA_agent_ddpg.py
@@ -156,8 +156,8 @@ class AgentDDPG():
 
         self.eps = args.eps_start
 
-        #self.criterion = nn.MSELoss()
-        self.criterion = lambda x,y:torch.mean(torch.log(torch.cosh(2*(x-y))))
+        self.criterion = nn.SmoothL1Loss()
+        #self.criterion = lambda x,y:torch.mean(torch.log(torch.cosh(2*(x-y))))
         self.optimizer_A = torch.optim.Adam(self.Anet.parameters(), lr=args.lr)
         self.optimizer_C = torch.optim.Adam(self.Cnet.parameters(), lr=args.lr)
 
